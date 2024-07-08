2980faec0bc8bdd65a72218452c6ce41d74025b2
ShinHyungJune <ssa4141@naver.com>
2024-07-08 14:36:05 +0900

    채팅 업로드

--- components/Empty.vue
+++ components/Empty.vue
@@ -4,8 +4,8 @@
 <style>
 .m-empty.type01 {
     padding:60px; margin:40px 0;
-    font-size:20px; font-weight:500; text-align: center; color:#999;
-    border-top:1px solid #e1e1e1; border-bottom:1px solid #e1e1e1;
+    font-size:16px; font-weight:500; text-align: center; color:#999;
+    border-top:1px solid #e1e1e1; border:1px solid #e1e1e1;
 }
 </style>
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,5 @@
 <template>
-  <div class="login-form">
-    로그인  페이지
-    <form @submit.prevent="login">
-      <div class="inputs">
-        <div class="input-text">
-          <input type="text" placeholder="아이디를 입력해주세요." v-model="form.email">
-          <error :form="form" name="email" />
-        </div>
-        <div class="input-text">
-          <input type="password" placeholder="비밀번호를 입력해주세요." v-model="form.password">
-          <error :form="form" name="password" />
-        </div>
 
-        <button class="btn" type="submit">Login</button>
-      </div>
-    </form>
-  </div>
 </template>
 <style>
 
