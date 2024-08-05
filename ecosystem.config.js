b0b36366cb1e1dd9d3831f8448ce81ad35bbfe1c
ShinHyungJune <ssa4141@naver.com>
2024-08-05 12:15:09 +0900

    fix .env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'industrialmarket.biz',
+            name: process.env.SITE_NAME || 'industrialmarket.biz',
             script: './node_modules/nuxt/bin/nuxt.js',
             args: 'start',
             port: process.env.PORT || 3025, // yoru app port
